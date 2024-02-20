111496dc6ab9c377ee807cdc76fce4f5255ca183
Filip Razvan <filip.razvan@agilefreaks.com>
2024-02-20 21:52:39 +0200

    Update header style

--- components/common/header/index.js
+++ components/common/header/index.js
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Appbar } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { Appbar, Avatar } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
 
 const CustomHeader = ({ navigation, back }) => {
   return (
-    <Appbar.Header>
-      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-      <Appbar.Content title="Buzz Buddy" titleStyle={styles.title} />
-      <Appbar.Action icon="account" onPress={() => {}} />
+    <Appbar.Header style={styles.header}>
+      {back ? (
+        <Appbar.BackAction onPress={navigation.goBack} color="white" />
+      ) : null}
+      <Appbar.Content title="MEETY" titleStyle={styles.logo} />
+      <View style={styles.profileIconContainer}>
+        <Avatar.Icon
+          icon="account"
+          color={'white'}
+          size={40}
+          style={styles.profileIcon}
+        />
+      </View>
     </Appbar.Header>
   );
 };
 
 const styles = StyleSheet.create({
-  title: {
-    fontFamily: 'MochiyPopOne',
+  logo: {
+    fontFamily: 'Sarina',
+    fontSize: 24,
+    color: '',
+  },
+  header: {
+    backgroundColor: '#8C1111',
+  },
+  profileIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  profileIcon: {
+    backgroundColor: 'gray',
+    borderWidth: 2,
+    borderColor: '#FBF4F4',
   },
 });
 
