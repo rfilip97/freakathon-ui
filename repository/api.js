cd5aa005524faa2a6620ce9a0631758511f54c4f
Filip Razvan <filip.razvan@agilefreaks.com>
2024-03-08 21:34:04 +0200

    Add repository

---
+++ repository/api.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'http://0.0.0.0:8090';
+
+async function apiCall(endpoint, options) {
+  try {
+    const response = await fetch(`${BASE_URL}/${endpoint}`, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default apiCall;
---
+++ repository/index.js
@@ -0,0 +1,15 @@
+import apiCall from './api';
+
+const login = async (identity, password) => {
+  return apiCall('api/collections/users/auth-with-password', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ identity, password }),
+  });
+};
+
+export const Repository = {
+  login,
+};
