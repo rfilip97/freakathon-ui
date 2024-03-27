f7eb9822a498879fa9fe85669d36292c4984d0ce
Filip Razvan <filip.razvan@agilefreaks.com>
2024-03-27 22:23:47 +0200

    Fix error if empty response from BE

--- repository/api.js
+++ repository/api.js
@@ -3,10 +3,11 @@ const BASE_URL = 'http://0.0.0.0:8090';
 async function apiCall(endpoint, options) {
   try {
     const response = await fetch(`${BASE_URL}/${endpoint}`, options);
-    if (!response.ok) {
+    if (response && !response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return await response.json();
+    const text = await response.text();
+    return text ? JSON.parse(text) : {};
   } catch (error) {
     throw error;
   }
